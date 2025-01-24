e0044b0f7a902dfcaa8359ee05bb6b0604ce1b98
273* <keita.y310@icloud.com>
2025-01-25 01:01:45 +0900

    ✨ OpenAIのレスポンスに対応しました．

---
+++ functions/src/fetchAIResponse/fetchOpenAIResponse.ts
@@ -0,0 +1,31 @@
+import OpenAI from "openai";
+
+import * as dotenv from "dotenv";
+
+// .envファイルの読み込み
+dotenv.config();
+
+// OpenAIの初期化
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// モデルの指定
+const model = process.env.OPENAI_MODEL as string;
+
+// OpenAIのレスポンスを取得する関数
+export const fetchOpenAIResponse = async (input: string, prompt: string) => {
+  // プロンプトとチャットの入力を用いてAIのレスポンスを取得
+  const response = await openai.chat.completions.create({
+    model,
+    messages: [
+      { role: "system", content: prompt },
+      { role: "user", content: input },
+    ],
+  });
+
+  // レスポンス結果を取得
+  const result = response.choices[0].message.content;
+
+  return String(result);
+};
--- functions/src/routes/index.ts
+++ functions/src/routes/index.ts
@@ -1,10 +1,25 @@
 import express, { Request, Response } from "express";
+import { fetchOpenAIResponse } from "../fetchAIResponse/fetchOpenAIResponse";
 
 const router = express.Router();
 
 router
-  .get("/hello", (req: Request, res: Response) => {
-    res.send("Hello Express!!");
+  .get("/openai", async (req: Request, res: Response) => {
+    // OpenAIのレスポンスを取得
+    try {
+      const ai_res = await fetchOpenAIResponse(
+        "マークダウンで回答して",
+        "あなたの機能について簡単に教えて"
+      );
+      res.status(200).json({
+        message: ai_res,
+      });
+    } catch (error: any) {
+      res.status(500).json({
+        error: "OpenAIからのレスポンス取得に失敗しました。",
+        details: error.message,
+      });
+    }
   })
   .get("/goodbye", (req: Request, res: Response) => {
     res.send("Goodbye Express!");
